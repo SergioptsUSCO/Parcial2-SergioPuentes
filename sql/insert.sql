f1adf98d424ce27b8009db58e8eb681ee53e9370
Sergio Puentes <u20242228195@usco.edu.co>
2026-04-27 18:22:46 -0500

    Query SQL básico para agregar usuarios y tipos de vehiculos

---
+++ sql/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO tipo_vehiculo (nombre) VALUES
+('Carro'),
+('Moto'),
+('Camión');
+
+INSERT INTO usuario (username, password, rol) VALUES
+('admin', '{noop}1234', 'ADMINISTRADOR'),
+('acomodador', '{noop}1234', 'ACOMODADOR'),
+('cliente', '{noop}1234', 'CLIENTE');
